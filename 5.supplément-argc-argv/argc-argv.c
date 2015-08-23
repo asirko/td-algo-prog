444a62d6d0c3f101588ac8c439ecee656137aa63
asirko <sirko.alexandre@gmail.com>
2015-08-24 00:38:44 +0200

    ajustement du texte des printf dans argc

--- 5.supplément-argc-argv/argc-argv.c
+++ 5.supplément-argc-argv/argc-argv.c
@@ -5,14 +5,14 @@
 int main(int argc, char * argv[]) {
     int i;
 
-    printf("\nVous avez tapez une commande avec %d argument(s).\n", argc);
+    printf("\nVous avez tapez une commande avec %d (=argc) argument(s).\n", argc);
 
     printf("\nLa commande tapée est : \n\t");
     for (i = 0; i < argc; i++) {
         printf("%s ", argv[i]);
     }
 
-    printf("\n\nLes arguments sont donc : \n");
+    printf("\n\nLes arguments dans argv sont donc : \n");
     for (i = 0; i < argc; i++) {
         printf(" - index %d : %s\n", i, argv[i]);
     }
